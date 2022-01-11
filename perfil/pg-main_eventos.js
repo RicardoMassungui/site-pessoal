1cf14ce483ed590914a833997628f7e975cca7e8
Ricardo casa <jonhricardo949@gmail.com>
2022-01-11 20:03:34 +0100

    foto

---
+++ perfil/pg-main_eventos.js
@@ -0,0 +1,20 @@
+// imagens da div slider
+function moverf(){
+    let evento = document.querySelector('img#fundo')
+     evento.src='imagens/ricardo-fp_2.png.jpg'
+ 
+ }
+ function sairf(){
+     let evento = document.querySelector('img#fundo')
+     evento.src='imagens/ricardo-fp.png.jpg'
+ }
+ // imagens da div slider
+function moverp(){
+    let evento = document.querySelector('f3')
+     evento.src='imagens/ricardo-fp_2.png.jpg'
+ 
+ }
+ function sairp(){
+     let evento = document.querySelector('f3')
+     evento.src='imagens/ricardo-fp.png.jpg'
+ }
